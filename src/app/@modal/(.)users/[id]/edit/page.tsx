b3daf1d1e11935535d37c8a99a787a9fe38c87f9
uixmat <mattsince87@gmail.com>
2024-11-03 14:47:24 +0200

    upgraded async request api's

--- src/app/@modal/(.)users/[id]/edit/page.tsx
+++ src/app/@modal/(.)users/[id]/edit/page.tsx
@@ -11,8 +11,13 @@ export const metadata: Metadata = {
   title: "Edit user",
 };
 
-export default async function EditUser({ params }: { params: { id: string } }) {
-  const user = await getUserById(params.id);
+export default async function EditUser({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const user = await getUserById(id);
 
   if (!user) {
     return notFound();
